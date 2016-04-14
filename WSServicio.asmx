468da5a6a28b759e6f94576fe20baa37d88b0b17
davidazure <davidse@outlook.com>
2016-04-14 14:06:12 +0200

    Added files via upload

---
+++ WSServicio.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WSServicio.asmx.cs" Class="WSAzure.WSServicio" %>
---
+++ WSServicio.asmx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WSAzure
+{
+    /// <summary>
+    /// Summary description for WSServicio
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WSServicio : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int suma(int dato1, int dato2)
+        {
+            return (dato1 + dato2);
+        }
+    }
+}
